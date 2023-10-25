b42d56a8290d66c73ffb3d7dcba022a58aaf4596
aDvernichuk <advernichuk@gmail.com>
2023-10-25 17:46:06 -0600

    added authentication system

--- demacia/urls.py
+++ demacia/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from home.views import signup, signin, signout
 
 urlpatterns = [
+    path('', include('home.urls')),
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),
+    path('home/signup/', signup, name ='signup'),
+    path('home/signin/', signin, name = 'login'),
+    path('home/signout/', signout, name = 'logout'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Game
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 def index(request):
-    games_list = Game.objects.order_by("gamename")
-    context = {"games_list" : games_list}
-    return render(request, "home/index.html", context)
+    if request.user.is_authenticated:
+        games_list = Game.objects.order_by("gamename")
+        context = {"games_list" : games_list}
+        return render(request, "home/index.html", context)
+    else:
+        return redirect('/home/signin')
 
 def blackjack(request):
     return HttpResponse("This is blackjack!")
@@ -15,3 +20,52 @@ def slots(request):
 
 def wheel(request):
     return HttpResponse("This is the free credit wheel!")
+
+def signup(request):
+
+    if request.user.is_authenticated:
+        return redirect('/home')
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username = username,password = password)
+            login(request, user)
+            return redirect('/home')
+
+        else:
+            return render(request,'home/signup.html',{'form':form})
+
+    else:
+        form = UserCreationForm()
+        return render(request,'home/signup.html',{'form':form})
+
+
+def signin(request):
+    if request.user.is_authenticated:
+        return redirect('/home')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username =username, password = password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('/home')
+        else:
+            form = AuthenticationForm()
+            return render(request,'home/signin.html',{'form':form})
+
+    else:
+        form = AuthenticationForm()
+        return render(request, 'home/signin.html', {'form':form})
+
+
+def signout(request):
+    logout(request)
+    return redirect('/home/signin/')
