6cac3750daec6367eddf962fbe048d44675b9bbb
aDvernichuk <advernichuk@gmail.com>
2023-11-01 00:54:39 -0600

    credits added, can change number through admin panel

--- home/admin.py
+++ home/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from .models import Game
+from .models import Game, Profile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
 # Register your models here.
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = "profile"
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline]
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Game)
--- home/models.py
+++ home/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    credits = models.DecimalField(max_digits=22, decimal_places=2)
+
 class Game(models.Model):
     gamename = models.CharField(max_length=50)
 
