152b3b1e1f0d08f331673eec76b1575f208cef90
THESYRUPKIDD <adenjohn318@gmail.com>
2023-10-31 20:01:46 -0600

    Tried to connect static folder (not successful)

--- demacia/settings.py
+++ demacia/settings.py
@@ -120,6 +120,8 @@ STATIC_ROOT = BASE_DIR /  'productionfiles'
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
