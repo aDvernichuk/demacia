3937021b7097a0dcff1b1cd58317f8fb88370760
aDvernichuk <advernichuk@gmail.com>
2023-11-12 13:44:53 -0600

    pushing new production files

---
+++ productionfiles/indexJava.js
@@ -0,0 +1,19 @@
+function bjText(){
+    document.getElementById("itext").innerHTML="Ready for the real game? Click here to challenge the blackjack table.";
+}
+
+function slText(){
+    document.getElementById("itext").innerHTML="Ready to Spin to Win? Click here to spin the slot machine.";
+}
+
+function frText(){
+    document.getElementById("itext").innerHTML="Want to win some more credits? Click here to spin the free credit wheel.";
+}
+
+function addText(){
+    document.getElementById("itext").innerHTML="Ran out of credits? Click here to add more to your account.";
+}
+
+function pText(){
+    document.getElementById("itext").innerHTML="To learn more about a game, hover one of the buttons on the menu."
+}
