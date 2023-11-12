9f65ab6ed11ab9fe50446700d15dd4283e99e89c
THESYRUPKIDD <adenjohn318@gmail.com>
2023-11-12 13:29:18 -0600

    updated js

--- home/static/indexJava.js
+++ home/static/indexJava.js
@@ -1,21 +1,19 @@
-var t= document.getElementById("itext");
-
 function bjText(){
-    t.innerHTML="Ready for the real game? Click here to challenge the blackjack table.";
+    document.getElementById("itext").innerHTML="Ready for the real game? Click here to challenge the blackjack table.";
 }
 
 function slText(){
-    t.innerHTML="Ready to Spin to Win? Click here to spin the slot machine.";
+    document.getElementById("itext").innerHTML="Ready to Spin to Win? Click here to spin the slot machine.";
 }
 
 function frText(){
-    t.innerHTML="Want to win some more credits? Click here to spin the free credit wheel.";
+    document.getElementById("itext").innerHTML="Want to win some more credits? Click here to spin the free credit wheel.";
 }
 
 function addText(){
-    t.innerHTML="Ran out of credits? Click here to add more to your account.";
+    document.getElementById("itext").innerHTML="Ran out of credits? Click here to add more to your account.";
 }
 
 function pText(){
-    t.innerHTML="To learn more about a game, hover one of the buttons on the menu."
+    document.getElementById("itext").innerHTML="To learn more about a game, hover one of the buttons on the menu."
 }
